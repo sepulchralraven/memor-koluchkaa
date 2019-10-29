407928ba6d4a61b1e7568bb3ea0879ce758d0125
sepulchralraven <55181930+sepulchralraven@users.noreply.github.com>
2019-10-29 21:57:53 +0300

    Add files via upload

---
+++ LABS 11 (недоделано)/LABA 11.3.cpp
@@ -0,0 +1,24 @@
+﻿// LABA 11.3.cpp 
+// Дано целое число N (> 1). Вывести наименьшее из целых чисел K,
+// для которых сумма 1 + 2 + . . . + K будет больше или равна N,
+// и саму эту сумму.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	unsigned int N, K;
+	int sum = 0;
+	cout << "Введите N" << endl;
+	cin >> N;
+	for (int i = 1; sum + i <= N; i++) {
+		sum = sum + i;
+		K = i;
+	}
+	cout << "Сумма чисел равна: " << sum << endl;
+	cout << "Последнее число в сумме равно: " << K;
+	return 0;
+}
+
