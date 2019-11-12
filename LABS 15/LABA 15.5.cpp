822ed58cee9db62cf7604a011523d72a07d1e41b
sepulchralraven <55181930+sepulchralraven@users.noreply.github.com>
2019-11-12 19:56:26 +0300

    gg

---
+++ LABS 15/LABA 15.5.cpp
@@ -0,0 +1,40 @@
+﻿// LABA 15.5.cpp 
+// Дан массив размера N, все элементы которого, кроме первого, упорядочены по возрастанию.
+// Сделать массив упорядоченным, переместив первый элемент на новую позицию.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, x;
+	int* arr;
+
+	cout << "Enter array's size: " << endl;
+	cin >> N;
+
+	arr = new int[N];
+
+	for (int i = 0; i < N; i++) {
+		cout << "arr[" << i << "] = ";
+		cin >> arr[i];
+	}
+
+	for (int i = 1; i < N; i++) {
+		x = arr[i];
+		int j = i - 1;
+		while (x < arr[j]) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = x;
+	}
+
+	cout << endl;
+	cout << "Ordered array: " << endl;
+	for (int i = 0; i < N; i++) 
+		cout << "arr[" << i << "] = " << arr[i] << endl;
+
+	return 0;
+}
+
